a176a04bdc15c7e0f54d55db2e0776d901b53bf9
SantiZambrana <santi.97.zambrana@gmail.com>
2020-10-12 19:29:20 -0300

    PaqueteServicios

--- src/main/java/com/demo/repositories/AlumnoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.demo.repositories;
-
-import com.demo.entities.Alumno;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AlumnoRepository extends BaseRepository <Alumno, Long>{
-}
--- src/main/java/com/demo/services/AlumnoService.java
+++
@@ -1,8 +0,0 @@
-package com.demo.services;
-
-import com.demo.entities.Alumno;
-import org.springframework.stereotype.Service;
-
-@Service
-public interface AlumnoService extends BaseService<Alumno, Long>{
-}
